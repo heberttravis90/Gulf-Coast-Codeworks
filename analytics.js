9dc77be68709b3e94893f9540389344de472d0fa
heberttravis90 <heberttravis90@gmail.com>
2026-09-14 13:33:10 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
 (function(){
-  var id=''; // Paste GA4 Measurement ID here, e.g. G-XXXXXXXXXX
+  var id='G-N4VL4YLV46';
   if(!id) return;
   var s=document.createElement('script');s.async=true;s.src='https://www.googletagmanager.com/gtag/js?id='+encodeURIComponent(id);document.head.appendChild(s);
   window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}window.gtag=gtag;gtag('js',new Date());gtag('config',id);
